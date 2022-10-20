86c50bf4d729aa304de4669850887b12b9193cd7
harrr <alinaami@mail.ru>
2022-10-20 18:09:38 +0300

    added fixtures

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -1,6 +1,12 @@
 import pytest
 from stuff.accum import Accumulator
 
+@pytest.fixture
+def accum():
+    return Accumulator()
+
+def test_accumulator_init_fixture(accum):
+    assert accum.count == 0
 
 def test_accumulator_init():
     accum = Accumulator()
@@ -12,12 +18,24 @@ def test_accumulator_add_one():
     accum.add()
     assert accum.count == 1
 
+def test_accumulator_add_one_fixture(accum):
+    accum.add()
+    assert accum.count == 1
+
 def test_accumulator_add_three():
     accum = Accumulator()
     accum.add(3)
     assert accum.count == 3
 
+def test_accumulator_add_three_fixture(accum):
+    accum.add(3)
+    assert accum.count == 3
+
 def test_accumulator_cannot_set_count_directly():
     accum = Accumulator()
+    with pytest.raises(AttributeError, match="can't set attribute") as e:
+        accum.count = 10
+
+def test_accumulator_cannot_set_count_directly_fixture(accum):
     with pytest.raises(AttributeError, match="can't set attribute") as e:
         accum.count = 10
